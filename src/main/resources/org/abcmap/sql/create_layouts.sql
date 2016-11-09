32f27515e667cd05a327d0961a357d0ce6b8e4e0
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-11-09 17:53:42 +0100

    Refactor sql scripts

---
+++ src/main/resources/org/abcmap/sql/create_layouts.sql
@@ -0,0 +1,5 @@
+CREATE TABLE abcmap_layouts (
+  number   INTEGER PRIMARY KEY NOT NULL,
+  format   CHAR(100)           NOT NULL,
+  position CHAR(100)           NOT NULL,
+);
---
+++ src/main/resources/org/abcmap/sql/create_project_metadata.sql
@@ -0,0 +1,4 @@
+CREATE TABLE abcmap_project_metadata (
+  md_key   CHAR(100) PRIMARY KEY NOT NULL,
+  md_value TEXT                  NOT NULL
+);
