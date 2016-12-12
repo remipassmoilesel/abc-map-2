8671b7a79b518bc5ac782e8704e3a6ee71e80c10
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-12-12 23:46:22 +0100

    Add argument parsing

--- src/main/java/org/abcmap/Launcher.java
+++ src/main/java/org/abcmap/Launcher.java
@@ -7,13 +7,40 @@ import org.abcmap.core.managers.ProjectManager;
 import org.abcmap.gui.GuiColors;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Launcher {
+
+    private static final String PANDA_MODE_ARG = "--panda-mode";
+    private static final String CREATE_FAKE_ARG = "--create-fake-project";
 
     public static void main(String[] args) throws IOException {
 
         System.out.println("Initializing Abc-Map, please wait ...");
-        System.out.println("Lancement d'Abc-Map, veuillez patienter...");
+        System.out.println();
+
+        // parse arguments
+        boolean devMode = false;
+        boolean createFake = false;
+        Path projectToOpen = null;
+        if (args != null && args.length > 0) {
+            for (String str : args) {
+                if (PANDA_MODE_ARG.equalsIgnoreCase(str)) {
+                    devMode = true;
+                    System.out.println("Panda mode activated");
+                } else if (CREATE_FAKE_ARG.equalsIgnoreCase(str)) {
+                    createFake = true;
+                    System.out.println("Fake project will be created");
+                } else {
+                    projectToOpen = Paths.get(str);
+                    System.out.println("Project will be opened: " + str);
+                }
+            }
+
+        }
+
+        MainManager.setDebugMode(devMode);
 
         try {
             MainManager.init();
@@ -21,15 +48,25 @@ public class Launcher {
             LaunchError.showErrorAndDie();
         }
 
+        // gui initialization
         GuiManager guim = MainManager.getGuiManager();
         guim.configureUiManager();
-
         GuiColors.init();
 
         ProjectManager pman = MainManager.getProjectManager();
 
-        // create a new project at launch
-        pman.createNewProject();
+        // open specified project
+        if (projectToOpen != null) {
+            pman.openProject(projectToOpen);
+        }
+        // create a fake project
+        else if (createFake) {
+            pman.createFakeProject();
+        }
+        // or create a new project at launch
+        else {
+            pman.createNewProject();
+        }
 
         try {
             MainManager.getGuiManager().constructAndShowGui();
