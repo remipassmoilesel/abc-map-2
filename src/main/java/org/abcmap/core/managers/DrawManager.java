c9a9bc05c86b1a533371b95bef6a2dc2b1e21490
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-12-19 19:17:34 +0100

    Draw manager: Add change tools method

--- src/main/java/org/abcmap/core/managers/DrawManager.java
+++ src/main/java/org/abcmap/core/managers/DrawManager.java
@@ -1,12 +1,16 @@
 package org.abcmap.core.managers;
 
 import org.abcmap.core.draw.*;
+import org.abcmap.core.events.DrawManagerEvent;
 import org.abcmap.core.events.manager.EventNotificationManager;
 import org.abcmap.core.events.manager.HasEventNotificationManager;
 import org.abcmap.core.project.layers.AbstractLayer;
 import org.abcmap.core.project.layers.FeatureLayer;
 import org.abcmap.core.styles.StyleContainer;
-import org.abcmap.gui.tools.ToolContainer;
+import org.abcmap.gui.components.map.CachedMapPane;
+import org.abcmap.gui.tools.MapTool;
+import org.abcmap.gui.tools.containers.ToolContainer;
+import org.abcmap.gui.tools.containers.ToolLibrary;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -16,7 +20,7 @@ import java.util.ArrayList;
  */
 public class DrawManager implements HasEventNotificationManager {
 
-    private final ProjectManager projectMan;
+    private final ProjectManager projectm;
 
     /**
      * Present thick of lines, used if a new builder is returned
@@ -32,12 +36,18 @@ public class DrawManager implements HasEventNotificationManager {
      * Active color used to draw, if a new builder is returned
      */
     private Color activeBackground;
-    private EventNotificationManager notifm;
+
+    /**
+     * Current tool activated
+     */
     private ToolContainer currentToolContainer;
+
+    private EventNotificationManager notifm;
 
     public DrawManager() {
 
-        this.projectMan = MainManager.getProjectManager();
+        this.projectm = MainManager.getProjectManager();
+
         this.activeForeground = Color.blue;
         this.activeBackground = Color.green;
         this.activeThick = 5;
@@ -87,22 +97,47 @@ public class DrawManager implements HasEventNotificationManager {
         return builder;
     }
 
+    /**
+     * Return active style on map
+     *
+     * @return
+     */
     public StyleContainer getActiveStyle() {
-        return projectMan.getProject().getStyle(activeForeground, activeBackground, activeThick);
+        return projectm.getProject().getStyle(activeForeground, activeBackground, activeThick);
     }
 
+    /**
+     * Return active foreground color used for drawing
+     *
+     * @return
+     */
     public Color getActiveForeground() {
         return activeForeground;
     }
 
+    /**
+     * Set active foreground color used for drawing
+     *
+     * @param activeForeground
+     */
     public void setActiveForeground(Color activeForeground) {
         this.activeForeground = activeForeground;
     }
 
+    /**
+     * Get active background color used for drawing
+     *
+     * @return
+     */
     public Color getActiveBackground() {
         return activeBackground;
     }
 
+    /**
+     * Set active background color used for drawing
+     *
+     * @return
+     */
     public void setActiveBackground(Color activeBackground) {
         this.activeBackground = activeBackground;
     }
@@ -113,15 +148,89 @@ public class DrawManager implements HasEventNotificationManager {
      * @return
      * @throws DrawManagerException
      */
-    public FeatureLayer getActiveFeatureLayerOrThrow() throws DrawManagerException {
+    private FeatureLayer getActiveFeatureLayerOrThrow() throws DrawManagerException {
 
-        AbstractLayer layer = projectMan.getProject().getActiveLayer();
+        AbstractLayer layer = projectm.getProject().getActiveLayer();
         if (layer instanceof FeatureLayer == false) {
             throw new DrawManagerException("Active layer is not a feature layer");
         }
 
         return (FeatureLayer) layer;
 
+    }
+
+    /**
+     * Set current tool
+     * <p>
+     * If new tool is set, return true. If not (because there is no map), return false.
+     *
+     * @param toolId
+     * @return
+     */
+    public boolean setCurrentTool(String toolId) {
+
+        ToolContainer toolCtr = null;
+        for (ToolContainer ctr : ToolLibrary.getAvailableTools()) {
+            if (ctr.getId().equals(toolId)) {
+                toolCtr = ctr;
+                break;
+            }
+        }
+
+        if (toolCtr == null) {
+            throw new IllegalArgumentException("Unknown id: " + toolId);
+        }
+
+        return setCurrentTool(toolCtr);
+    }
+
+    /**
+     * Set current tool
+     * <p>
+     * If new tool is set, return true. If not (because there is no map), return false.
+     *
+     * @param toolContainer
+     * @return
+     */
+    public boolean setCurrentTool(ToolContainer toolContainer) {
+
+        CachedMapPane map = MainManager.getMapManager().getMainMap();
+
+        // stop previous tool if needed
+        if (currentToolContainer != null) {
+            MapTool previousTool = currentToolContainer.getCurrentInstance();
+            if (previousTool != null) {
+                previousTool.stopWorking();
+
+                if (map != null) {
+                    map.removeToolFromListeners(previousTool);
+                }
+            }
+        }
+
+        // if map is not present, return false
+        if (map == null) {
+            return false;
+        }
+
+        // else, register tool as listener
+        this.currentToolContainer = toolContainer;
+        MapTool instance = toolContainer.getNewInstance();
+        map.addToolToListeners(instance);
+
+        // throw event
+        notifm.fireEvent(new DrawManagerEvent(DrawManagerEvent.TOOL_CHANGED));
+
+        return true;
+    }
+
+    /**
+     * Return current tool container
+     *
+     * @return
+     */
+    public ToolContainer getCurrentToolContainer() {
+        return currentToolContainer;
     }
 
     public ArrayList<String> getAvailableSymbolSets() {
@@ -165,11 +274,5 @@ public class DrawManager implements HasEventNotificationManager {
         return notifm;
     }
 
-    public void setCurrentTool(ToolContainer currentTool) {
 
-    }
-
-    public ToolContainer getCurrentToolContainer() {
-        return currentToolContainer;
-    }
 }
