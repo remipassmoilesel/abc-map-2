eaa6a2474c8b07740cce1ceb860732eb48a841d8
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-12-30 16:04:33 +0100

    Improve handles

--- src/main/java/org/abcmap/gui/shapes/Handle.java
+++ src/main/java/org/abcmap/gui/shapes/Handle.java
@@ -1,28 +1,33 @@
 package org.abcmap.gui.shapes;
 
-import org.abcmap.core.managers.DrawManager;
-import org.abcmap.core.managers.MainManager;
-
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
-import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * Handle which show on map that interactions are possible.
  */
 public class Handle {
 
+    /**
+     * Handle used to move objects
+     */
     public static final String FOR_MOVING = "FOR_MOVING";
+
+    /**
+     * Handle used to resize objects
+     */
     public static final String FOR_RESIZING = "FOR_RESIZING";
 
     private static final Color COLOR_FOR_MOVING = new Color(51, 215, 241);
     private static final Color COLOR_FOR_RESIZING = new Color(227, 84, 16);
     private static final Color COLOR_FOR_SHADOW = Color.lightGray;
 
-    private static final int WIDTH = 17;
-    private static final int GAP = 2;
-    private static final int IN_THICK = 6;
-    private static final int OUT_THICK = 4;
+    private int totalWidth;
+    private int gapBetweenShapes;
+    private int inThick;
+    private int outThick;
+    private int interactionAreaMarginPx;
 
     private Ellipse2D shpOut;
     private Ellipse2D shpIn;
@@ -30,27 +35,40 @@ public class Handle {
     private String type;
     private Color color;
 
+    /**
+     * Area where interactions are
+     */
     private Rectangle interactionArea;
 
     private boolean drawInteractionArea;
-    private DrawManager drawm;
 
     public Handle(String type) {
 
-        this.drawm = MainManager.getDrawManager();
+        this.interactionAreaMarginPx = 5;
+        this.totalWidth = 17;
+        this.gapBetweenShapes = 2;
+        this.inThick = 6;
+        this.outThick = 4;
 
         this.drawInteractionArea = false;
 
         // position of handle on panel
         this.position = new Point();
 
+        // this handle will be used to move
         if (FOR_MOVING.equals(type)) {
             this.type = FOR_MOVING;
             this.color = COLOR_FOR_MOVING;
-        } else if (FOR_RESIZING.equals(type)) {
+        }
+
+        // this handle will be used to resize
+        else if (FOR_RESIZING.equals(type)) {
             this.type = FOR_RESIZING;
             this.color = COLOR_FOR_RESIZING;
-        } else {
+        }
+
+        // invalid arg
+        else {
             throw new IllegalArgumentException("Invalid type: " + type);
         }
 
@@ -67,11 +85,11 @@ public class Handle {
 
         // white border for dark backgrounds
         g.setColor(COLOR_FOR_SHADOW);
-        g.setStroke(new BasicStroke(OUT_THICK));
+        g.setStroke(new BasicStroke(outThick));
         g.draw(shpOut);
 
         g.setColor(color);
-        g.setStroke(new BasicStroke(IN_THICK));
+        g.setStroke(new BasicStroke(inThick));
         g.draw(shpIn);
     }
 
@@ -80,13 +98,13 @@ public class Handle {
      */
     public void refreshShape() {
 
-        int halfWidthOut = Math.round(WIDTH / 2);
-        int widthIn = WIDTH - (GAP + OUT_THICK) * 2;
+        int halfWidthOut = Math.round(totalWidth / 2);
+        int widthIn = totalWidth - (gapBetweenShapes + outThick) * 2;
 
         int rx = position.x - halfWidthOut;
         int ry = position.y - halfWidthOut;
-        int rw = WIDTH;
-        int rh = WIDTH;
+        int rw = totalWidth;
+        int rh = totalWidth;
         shpOut = new Ellipse2D.Double(rx, ry, rw, rh);
 
         int r2x = position.x - widthIn / 2;
@@ -95,13 +113,21 @@ public class Handle {
         int r2h = widthIn;
         shpIn = new Ellipse2D.Double(r2x, r2y, r2w, r2h);
 
-        int margin = drawm.getInteractionAreaMargin();
-        int halfMargin = margin / 2;
+        int halfMargin = interactionAreaMarginPx / 2;
         Rectangle b = shpOut.getBounds();
 
-        interactionArea = new Rectangle(b.x - halfMargin, b.y - halfMargin, b.width + margin,
-                b.height + margin);
+        interactionArea = new Rectangle(b.x - halfMargin, b.y - halfMargin, b.width + interactionAreaMarginPx,
+                b.height + interactionAreaMarginPx);
 
+    }
+
+    /**
+     * Return true if point is on handle
+     * @param p
+     * @return
+     */
+    public boolean isPointOn(Point p) {
+        return interactionArea.contains(p);
     }
 
     public void setPosition(Point p) {
@@ -124,19 +150,36 @@ public class Handle {
         return type;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-
-        if (obj instanceof Handle == false)
-            return false;
-
-        Handle shp = (Handle) obj;
-
-        Object[] toCompare1 = new Object[]{this.position, this.type};
-        Object[] toCompare2 = new Object[]{shp.position, shp.type};
-
-        return Arrays.deepEquals(toCompare1, toCompare2);
-
+    public void setInteractionAreaMarginPx(int interactionAreaMarginPx) {
+        this.interactionAreaMarginPx = interactionAreaMarginPx;
     }
 
+    public int getInteractionAreaMarginPx() {
+        return interactionAreaMarginPx;
+    }
+
+    /**
+     * Only use position and type
+     *
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Handle handle = (Handle) o;
+        return Objects.equals(position, handle.position) &&
+                Objects.equals(type, handle.type);
+    }
+
+    /**
+     * Only use position and type
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, type);
+    }
 }
