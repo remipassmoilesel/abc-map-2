ec342fb22da0b144883ad0df9e91ae61383a5b2b
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-01-02 23:16:03 +0100

    Add try/c block to main window mode change on new project to prevent unecessary error messages

--- src/main/java/org/abcmap/gui/windows/MainWindow.java
+++ src/main/java/org/abcmap/gui/windows/MainWindow.java
@@ -111,17 +111,20 @@ public class MainWindow extends AbstractCustomWindow implements HasEventNotifica
 
             SwingUtilities.invokeLater(() -> {
 
-                // project closed, empty center
-                if (ProjectEvent.isCloseProjectEvent(notif)) {
-                    setWindowMode(MainWindowMode.EMPTY);
-                    logger.debug("Window mode updated");
-                }
+                try {
+                    // project closed, empty center
+                    if (ProjectEvent.isCloseProjectEvent(notif)) {
+                        setWindowMode(MainWindowMode.EMPTY);
+                        logger.debug("Window mode updated");
+                    }
 
-                // new project, show map and reset display
-                else if (ProjectEvent.isNewProjectLoadedEvent(notif)) {
-                    setWindowMode(MainWindowMode.SHOW_MAP);
-//                    mapPanel.resetDisplay();
-                    logger.debug("Window mode updated");
+                    // new project, show map and reset display
+                    else if (ProjectEvent.isNewProjectLoadedEvent(notif)) {
+                        setWindowMode(MainWindowMode.SHOW_MAP);
+                        logger.debug("Window mode updated");
+                    }
+                } catch (Exception e) {
+                    logger.debug(e);
                 }
 
             });
