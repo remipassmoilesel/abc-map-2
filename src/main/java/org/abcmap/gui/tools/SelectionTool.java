d57edc2a634ebc8de652d5e8efb106ef3dbde323
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-01-06 18:28:39 +0100

    Improve selection tool, add selection by area

--- src/main/java/org/abcmap/gui/tools/SelectionTool.java
+++ src/main/java/org/abcmap/gui/tools/SelectionTool.java
@@ -8,6 +8,7 @@ import org.abcmap.core.utils.GeoUtils;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.opengis.feature.simple.SimpleFeature;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -21,6 +22,11 @@ public class SelectionTool extends MapTool {
      * Features currently been selected
      */
     private final ArrayList<SimpleFeature> selectedFeatures;
+
+    /**
+     * Utility used to draw rectangles, for area selection
+     */
+    private final SimpleRectangleDesigner rectangleSelectionDesigner;
 
     /**
      * If true, features are moving
@@ -41,11 +47,16 @@ public class SelectionTool extends MapTool {
         super();
         moving = false;
         selectedFeatures = new ArrayList<>();
+        rectangleSelectionDesigner = new SimpleRectangleDesigner();
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         super.mouseClicked(e);
+
+        if (SwingUtilities.isLeftMouseButton(e) == false) {
+            return;
+        }
 
         // retrieve active layer
         AbmFeatureLayer activeLayer = getActiveFeatureLayerIfLeftClickOrShowMessage(e);
@@ -59,16 +70,17 @@ public class SelectionTool extends MapTool {
         // no features or error, cancel feature changes and return
         if (newSelection == null || newSelection.size() < 1) {
 
-            // cancel selecton and feature changes only if control NOT down
+            // cancel selection and feature changes only if control NOT down
             if (e.isControlDown() == false) {
-                cancelFeaturesChanges();
+                clearMemoryLayer();
                 selectedFeatures.clear();
+                repaintMainMap();
             }
 
             return;
         }
 
-        // ctrl not down: remove previous features
+        // ctrl not down: remove previous features from selection
         if (e.isControlDown() == false) {
             selectedFeatures.clear();
         }
@@ -86,24 +98,33 @@ public class SelectionTool extends MapTool {
     public void mouseDragged(MouseEvent e) {
         super.mouseDragged(e);
 
-        // first move: keep mouse position and return
-        if (lastMousePosition == null) {
-            lastMousePosition = e.getPoint();
+        if (SwingUtilities.isLeftMouseButton(e) == false) {
             return;
         }
 
         // we are not moving shapes now
         if (moving == false) {
 
-            // check if mouse is on a shape, if on start moving
+            // check if mouse is on a shape in memory, if on start moving
             ArrayList<SimpleFeature> featuresUnderMouse = getFeaturesFromMemoryAroundMousePosition(e.getPoint());
             if (featuresUnderMouse != null && featuresUnderMouse.size() > 0) {
                 moving = true;
+            }
+
+            // else draw a rectangle for multiple selection
+            if (moving == false) {
+                rectangleSelectionDesigner.mouseDragged(e);
             }
         }
 
         // we are moving, translate shapes
         if (moving == true) {
+
+            // first move: keep mouse position and return
+            if (lastMousePosition == null) {
+                lastMousePosition = e.getPoint();
+                return;
+            }
 
             // compute world unit move
             Point currentPos = e.getPoint();
@@ -128,19 +149,22 @@ public class SelectionTool extends MapTool {
                 g.geometryChanged();
             }
 
-            // repaint in memory layer map
-            repaintMainMap();
-
             // keep last position
             lastMousePosition = currentPos;
 
         }
+
+        repaintMainMap();
 
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         super.mouseReleased(e);
+
+        if (SwingUtilities.isLeftMouseButton(e) == false) {
+            return;
+        }
 
         // we was moving features, commit changes and refresh map
         if (moving && selectedFeatures.size() > 0) {
@@ -155,11 +179,63 @@ public class SelectionTool extends MapTool {
             // refresh map
             deleteActiveLayerCacheAndUpdateMap(boundsAfterMove);
 
-            // reset flags
-            lastMousePosition = null;
-            moving = false;
         }
+
+        // we was drawing a rectangle area for selection
+        else if (rectangleSelectionDesigner.isWorking()) {
+
+            // finish rectangle and get it
+            rectangleSelectionDesigner.mouseReleased(e);
+
+            // translate screen rectangle in world envelope
+            Rectangle screenSelection = rectangleSelectionDesigner.getRectangle();
+            ReferencedEnvelope worldSelection = screenRectangleToWorldEnvelope(screenSelection);
+
+            // get features in world envelope
+            ArrayList<SimpleFeature> features = getFeaturesFromProjectInEnvelope(worldSelection, false);
+
+            // remove old features if control is not down
+            if(e.isControlDown() == false){
+                selectedFeatures.clear();
+            }
+
+            // add feature in selection and in memory layer
+            selectedFeatures.addAll(features);
+            if(selectedFeatures.size() > 0){
+                setFeaturesModifiable(selectedFeatures);
+            }
+
+            // no features selected, clear in memory layer
+            else {
+                clearMemoryLayer();
+            }
+
+        }
+
+        // reset flags
+        lastMousePosition = null;
+        moving = false;
+        rectangleSelectionDesigner.resetRectangle();
+
+        repaintMainMap();
 
     }
 
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        super.mouseMoved(e);
+
+        // repaint main map to show rectangle selection if needed
+        if (rectangleSelectionDesigner.isWorking()) {
+            repaintMainMap();
+        }
+    }
+
+    @Override
+    public void drawOnMainMap(Graphics2D g2d) {
+        super.drawOnMainMap(g2d);
+
+        // draw rectangle selection if needed
+        rectangleSelectionDesigner.draw(g2d);
+    }
 }
