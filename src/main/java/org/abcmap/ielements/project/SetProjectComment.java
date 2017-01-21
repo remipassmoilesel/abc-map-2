ab8b3f349dd2dea52177848f1ca78c964fce44f0
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-01-21 10:34:54 +0100

    Restore project comment interaction element

--- src/main/java/org/abcmap/ielements/project/SetProjectComment.java
+++ src/main/java/org/abcmap/ielements/project/SetProjectComment.java
@@ -1,6 +1,11 @@
 package org.abcmap.ielements.project;
 
 import net.miginfocom.swing.MigLayout;
+import org.abcmap.core.events.ProjectEvent;
+import org.abcmap.core.project.PMConstants;
+import org.abcmap.core.utils.Utils;
+import org.abcmap.gui.components.textfields.TextFieldDelayedAction;
+import org.abcmap.gui.utils.GuiUtils;
 import org.abcmap.ielements.InteractionElement;
 import org.abcmap.gui.utils.FormUpdater;
 
@@ -22,76 +27,72 @@ public class SetProjectComment extends InteractionElement {
     @Override
     protected Component createPrimaryGUI() {
 
+        // create GUI with text field
         JPanel panel = new JPanel(new MigLayout("insets 0"));
-
         textField = new JTextArea();
-
         panel.add(new JScrollPane(textField), "width 80%, height 70px, grow");
 
+        // listen project changes
         projectListener = new ProjectListener();
+        projectListener.addEventNameFilter(ProjectEvent.METADATA_CHANGED);
+        projectListener.addEventNameFilter(ProjectEvent.NEW_PROJECT_LOADED);
 
         notifm.addEventListener(projectListener);
         projectm().getNotificationManager().addObserver(this);
 
-        //TextFieldDelayedAction.delayedActionFor(textField, new TextFieldListener(), false);
+        // listen user input
+        TextFieldDelayedAction.delayedActionFor(textField, new TextFieldListener(), false);
 
+        // first update
         projectListener.run();
 
         return panel;
 
     }
 
+    /**
+     * Listen project changes and change text field
+     */
     private class ProjectListener extends FormUpdater {
 
         @Override
         public void updateFormFields() {
 
-			/*
+            if (projectm().isInitialized() == false) {
+                GuiUtils.changeTextWithoutFire(textField, "");
+                return;
+            }
 
-			// projet non initialisé, retour
-			if (projectm.isInitialized() == false) {
-				GuiUtils.changeText(textField, "");
-				return;
-			}
-
-			// commentaire du projet
-			String projectComment = projectm.getMetadatas().PROJECT_COMMENT;
-
-			GuiUtils.changeText(textField, projectComment);
-
-			*/
+            String projectComment = projectm().getProject().getMetadataContainer().getValue(PMConstants.COMMENT);
+            GuiUtils.changeTextWithoutFire(textField, projectComment);
         }
 
     }
 
-
+    /**
+     * Listen user input and change project
+     */
     private class TextFieldListener implements Runnable {
 
         @Override
         public void run() {
 
-            /*
-
-
-            // Verifier le projet et obtenir le calque actif, ou afficher un
-            // message d'erreur
-            MapLayer layer = checkProjectAndGetActiveLayer();
-            if (layer == null) {
+            // project is not initialized, return
+            if (projectm().isInitialized() == false) {
                 return;
             }
 
-            // commentaire du projet
-            String projectComment = configm.getConfiguration().PROFILE_COMMENT;
+            // get title from project
+            String projectComment = projectm().getProject().getMetadataContainer().getValue(PMConstants.COMMENT);
 
-            // recuperer le titre saisi
+            // get input
             String fieldComment = textField.getText();
 
             if (Utils.safeEquals(projectComment, fieldComment) == false) {
-                projectm.getMetadatas().PROJECT_COMMENT = fieldComment;
-                projectm.fireMetadatasChanged();
+                projectm().getProject().getMetadataContainer().updateValue(PMConstants.COMMENT, fieldComment);
+                projectm().fireMetadatasChanged();
             }
 
-            */
 
         }
 
